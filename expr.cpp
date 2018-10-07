70353d3c662d66966aeae122e2b5caaa078cb557
Jacob Liddy <liddyjacob@gmail.com>
2018-10-06 21:17:32 -0400

    Basic printing for expressions complete

--- expr.cpp
+++ expr.cpp
@@ -21,8 +21,20 @@ void print(Printer& p, Expr* e){
   if (e->kind <= Expr::unEND){
     // TODO Print out details for unary expresisons.
     
+
+    switch (e->kind){
+      case Expr::addinv:
+        p << "addinv(-e)";
+        break;
+      case Expr::mulinv:
+        p << "mulinv(/e)";
+        break;
+      case Expr::negate:
+        p << "negate(!e)";
+        break;
+    }
   
-    p << "unary: \n";
+    p << "(unary):\n";
     p.indent += 1;
     print(p, static_cast<UnaryE*>(e)->expr);
     return;
@@ -32,8 +44,43 @@ void print(Printer& p, Expr* e){
   if (e->kind <= Expr::binEND){
     // TODO Print out details for unary expresisons.
     
-  
-    p << "binary: \n";
+
+    switch (e->kind){
+      case Expr::add:
+        p << "Add (e1 + e2)";
+        break;
+      case Expr::sub:
+        p << "Sub (e1 + e2)";
+        break;
+      case Expr::mul:
+        p << "Mul (e1 * e2)";
+        break;
+      case Expr::quo:
+        p << "Div (e1 / e2)";
+        break;
+      case Expr::rem:
+        p << "Rem (e1 % 32)";
+        break;
+      case Expr::lt:
+        p << "LT  (e1 < e2)";
+        break;
+      case Expr::le:
+        p << "LE  (e1 <= e2)";
+        break;
+      case Expr::gt:
+        p << "GT  (e1 > e2)";
+        break;
+      case Expr::ge:
+        p << "GE  (e1 >= e2)";
+        break;
+      case Expr::eq:
+        p << "EQ  (e1 == e2)";
+        break;
+      case Expr::neq:
+        p << "NEQ (e1 != e2)";
+        break;
+    }
+    p << "(binary): \n";   
     p.indent += 1;
     print(p, static_cast<BinaryE*>(e)->expr1);
     p << "\n";
--- value.hpp
+++ value.hpp
@@ -1,38 +1,49 @@
 #pragma once
-
+#include "printer.hpp"
 class Value
 {
 public:
   explicit Value(int n);
   explicit Value(bool b);
   explicit Value(float f);
+ 
+  friend Printer& operator<<(Printer& p, const Value& v);
+
   int get_int() const;
   double get_float() const;
   bool get_bool() const;
+  
+  enum Kind{
+    intV,
+    boolV,
+    floatV,
+  };
 
 private:
   //Current Lazy implementation does not use discriminated union (but should)
   int ival;
   float fval;
   bool bval;
+  Kind kind;
 };
+
+
 
 inline 
 Value::Value(int n)
-  : ival(n)
+  : ival(n), kind(intV)
 { }
 
 inline 
 Value::Value(bool b)
-  : bval(b)
+  : bval(b), kind(boolV)
 { }
 
 inline 
 Value::Value(float f)
-  : fval(f)
+  : fval(f), kind(floatV)
 { }
-
-  
+ 
 inline 
 int Value::get_int() const
 {
